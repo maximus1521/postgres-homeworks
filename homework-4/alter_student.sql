dcb964d8a04a8eb41456d8ec3a9bc253034355e0
Maximus <maximus1521@gmail.com>
2024-05-09 12:29:26 +0200

    1st try

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,30 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
+CREATE TABLE students
+(
+	student_id serial,
+	first_name varchar,
+	last_name varchar,
+	birthday date,
+	phone varchar
+)
 
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE students ADD COLUMN middle_name varchar
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE students DROP COLUMN middle_name
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE students RENAME birthday TO birth_date
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE students ALTER COLUMN phone SET DATA TYPE varchar(32)
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO students (first_name, last_name, birth_date, phone) VALUES ('Alfa', 'Bravo', '01.01.2001', 00001)
+INSERT INTO students (first_name, last_name, birth_date, phone) VALUES ('Charlie', 'Delta', '02.01.2021', 20001)
+INSERT INTO students (first_name, last_name, birth_date, phone) VALUES ('Echo', 'Foxtrot', '02.03.2031', 23003)
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE students RESTART IDENTITY
